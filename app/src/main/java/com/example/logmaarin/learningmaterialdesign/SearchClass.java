475daf520a5c0753d1070075f6adfdd80e283a02
isahin1997 <isahin1997@gmail.com>
2015-06-05 00:31:24 +0200

    tweets ophalen lukt, nu alleen nog de zoekresultaten in lezen met jsonparser

--- app/src/main/java/com/example/logmaarin/learningmaterialdesign/SearchClass.java
+++ app/src/main/java/com/example/logmaarin/learningmaterialdesign/SearchClass.java
@@ -2,17 +2,30 @@ package com.example.logmaarin.learningmaterialdesign;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Base64;
+import android.util.Log;
 import android.widget.SearchView;
+import android.widget.TextView;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Created by LogMaarIn on 2-6-2015.
@@ -21,37 +34,37 @@ import java.net.URLEncoder;
 
 public class SearchClass extends Activity {
 
-    private	static final String	API_KEY	= "8IRhj6hsyRl5mrU0Aj7n2eEEX";
-    private static final String	API_SECRET	= "2PR7M2x6M8vYpHdbtITjoVvM6bOwSh18R8xIT2wUPlzpYtUaZh";
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
+    String token;
     SearchView search;
+    private TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_layout);
         search = (SearchView) findViewById(R.id.searchView);
-        //	Plak	key	en	secret	aan	elkaar	en	zorg	dat	er	nooit	‘rare’
-        //	tekens	inzitten
-        String	authString	=	API_KEY +	":"	+	API_SECRET;
-        String	base64	=	Base64.encodeToString(
-                authString.getBytes(), Base64.NO_WRAP);
-        //	Maak een	POST-request
-        HttpPost request	=
-                new HttpPost("https://api.twitter.com/oauth2/token");
-        //	Voeg key+secret	toe
-        request.setHeader("Authorization",	"Basic	"	+	base64);
-        request.setHeader("Content-Type",
-                "application/x-www-form-urlencoded;charset=UTF-8");
-        //	Vul	body:	vertel dat	je	inlogt	met	app	key	en	secret
-        try {
-            request.setEntity(
-                    new StringEntity("grant_type=client_credentials"));
+        text = (TextView) findViewById(R.id.textView);
 
-        } catch (UnsupportedEncodingException e) {
+        try {
+            token = new TokenAsyncTask().execute().get();
+            Log.d("hoi", "gelukt!");
+            Log.d("hoi", token);
+        }
+        catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
         search.setOnQueryTextListener(new TextListener());
+
+
+
+
 
     }
 
@@ -59,24 +72,26 @@ public class SearchClass extends Activity {
 
         @Override
         public boolean onQueryTextSubmit(String query) {
-            return false;
+            String[] params = new String[]{query, token};
+            try {
+                String s = new SearchTask().execute(params).get();
+                text.setText(s);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+
+
+           return true;
         }
 
         @Override
         public boolean onQueryTextChange(String newText) {
 
-            try {
-                String s = URLEncoder.encode(newText , "UTF-8");
-                HttpGet httpGet =new HttpGet("https:/api.twitter.com/1.1/search/tweets.json?q=" + s);
-                httpGet.setHeader("Authorization", "Bearer " + token);
-                ResponseHandler<String> handler
-                        = new BasicResponseHandler();
-                String searchJSON = client.execute(httpGet, handler);
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
 
 
+            return false;
         }
     }
 }
